d1bb46feac1b8bd6620f688d92ec8d8e88f54dfd
Devilal panchal <devilalpanchal551@gmail.com>
2024-01-13 18:32:13 +0530

    devilal portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+/*============================== Typing
+animation=====================================*/
+var typed = new Typed(".typing", {
+    strings: [ " ","Web Designer", "Web Developer","Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+    });
