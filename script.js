2e29e85d1555b7bcd112cc435a39b3b166279ef8
Devilal panchal <devilalpanchal551@gmail.com>
2024-05-14 18:31:23 +0530

    resume

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ animation=====================================*/
 var typed = new Typed(".typing", {
     strings: [ " ","Web Designer", "Web Developer","Graphic Designer"],
     typeSpeed: 100,
-    backSpeed: 60,
+    backSpeed:60,
     loop: true
     });
